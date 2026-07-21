acf4c5e10d74b8c23f62d37acf922306e469194f
JendoAK <jenny.donaldson@outlook.com>
2026-07-21 08:47:43 -0800

    Simplify mobile header and filters

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "foodbrokerbase-app-v8";
+const CACHE_NAME = "foodbrokerbase-app-v9";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=20260720-3",
+  "./styles.css?v=20260721-1",
   "./app.js?v=20260720-1",
   "./supabase-config.js",
   "./supabase-auth.js?v=20260720-1",
