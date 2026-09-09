ebe5ff12daa5747df097b76dab9f7b687de2ff2c
JendoAK <jenny.donaldson@outlook.com>
2026-09-09 10:44:01 -0800

    Make add actions prominent and remove tracker sharing caption

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "foodbrokerbase-app-v47";
+const CACHE_NAME = "foodbrokerbase-app-v48";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=20260909-2",
+  "./styles.css?v=20260909-3",
   "./market-events.js?v=20260908-7",
   "./personal-visit-calendar.js?v=20260908-7",
   "./calendar-holidays.js?v=20260908-8",
