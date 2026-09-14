7cb85bdddddbc306004f2407a3ce876eacfdd5cf
JendoAK <jenny.donaldson@outlook.com>
2026-09-14 09:40:33 -0800

    Refresh cache for faster storage handling

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
-const CACHE_NAME = "foodbrokerbase-productnotes1";
+const CACHE_NAME = "foodbrokerbase-storagefast1";
 const APP_SHELL = [
   "./vendor/lz-string.js?v=1.5.0",
-  "./compact-storage.js?v=20260911-storage1",
+  "./compact-storage.js?v=20260914-storagefast1",
   "./todo-archive.js?v=20260909-todoarchive1",
   "./todo-archive.css?v=20260909-todoarchive1",
   "./lead-products.css?v=20260909-leadcompact1",
